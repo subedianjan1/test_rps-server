f2cddbab733a9869e3a01300e9a6b8ba747f7ef2
Anj <subedianjan@yahoo.com.au>
2019-08-06 18:23:41 +1000

    Initial tests for the power supply

--- test_rps-server.py
+++ test_rps-server.py
@@ -3,3 +3,43 @@ from command import *
 C = commands()
 C.send_cmds('SetPower', 3, 0)
 print (C.send_cmds('GetPower'))
+
+
+def test_port0():
+    #Trun ON
+    assert C.send_cmds('SetPower', 0, 1) == True
+    assert C.send_cmds('GetPower')== "P60=1P61=0P62=0P63=0"
+    #Trun OFF
+    assert C.send_cmds('SetPower', 0, 0) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=0P62=0P63=0"
+    #Check status
+    assert C.form_query(1,' ', 1) == {'P61': 1}
+    
+
+    
+def test_port1():
+    #Trun ON
+    assert C.send_cmds('SetPower', 1, 1) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=1P62=0P63=0"
+    #Trun OFF
+    assert C.send_cmds('SetPower', 1, 0) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=0P62=0P63=0"
+
+
+def test_port2():
+    #Trun ON
+    assert C.send_cmds('SetPower', 2, 1) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=0P62=1P63=0"
+    #Trun OFF
+    assert C.send_cmds('SetPower', 2, 0) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=0P62=0P63=0"
+
+
+def test_port3():
+    #Trun ON
+    assert C.send_cmds('SetPower', 3, 1) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=0P62=0P63=1"
+    #Trun OFF
+    assert C.send_cmds('SetPower', 3, 0) == True
+    assert C.send_cmds('GetPower')== "P60=0P61=0P62=0P63=0"
+
